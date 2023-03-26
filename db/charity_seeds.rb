b8df027fc9649cbf266eb62b4ac4c34cdad31233
Simon Hoefling <simon@hoeflia.eu>
2023-03-26 17:30:16 +0800

    General styleoffence fixes

--- db/charity_seeds.rb
+++ db/charity_seeds.rb
@@ -10,7 +10,8 @@ charities = [
   {
     name: "Give a Hand",
     address: "Phnom Penh, Cambodia",
-    description: "Works to improve the lives of marginalized communities through education, health, and sanitation programs.",
+    description: "Works to improve the lives of marginalized communities through education, health,
+    and sanitation programs.",
     cause: "Community Development",
     image: "give-a-hand.png"
   },
@@ -52,7 +53,8 @@ charities = [
   {
     name: "Wings of Hope",
     address: "Kampala, Uganda",
-    description: "Provides education and vocational training to young women, with a focus on empowering them to become leaders in their communities.",
+    description: "Provides education and vocational training to young women, with a focus on empowering
+    them to become leaders in their communities.",
     cause: "Women's Empowerment",
     image: "wings-of-hope.png"
   },
@@ -66,56 +68,67 @@ charities = [
   {
     name: "Food Bank",
     address: "Manila, Philippines",
-    description: "Collects and distributes surplus food to those in need, and works to address food insecurity in the country.",
+    description: "Collects and distributes surplus food to those in need, and works
+    to address food insecurity in the country.",
     cause: "Children and Youth",
     image: "food-bank.png"
   },
   {
     name: "Water for Life",
     address: "Kathmandu, Nepal",
-    description: "Works to improve access to safe water and sanitation in rural communities, with a focus on promoting hygiene and preventing disease.",
+    description: "Works to improve access to safe water and sanitation in rural communities, with a focus on promoting
+    hygiene and preventing disease.",
     cause: "Health",
     image: "water-for-life.png"
   },
   {
     name: "Hands Across Borders",
     address: "Caracas, Venezuela",
-    description: "Provides humanitarian assistance and support to refugees and migrants, and works to promote human rights and social justice.",
+    description: "Provides humanitarian assistance and support to refugees and migrants, and works
+    to promote human rights and social justice.",
     cause: "Community Development",
     image: "hand-across-borders.png"
   },
   {
     name: "Community Development",
     address: "Dakar, Senegal",
-    description: "Works to improve living conditions and economic opportunities in urban and rural communities, through infrastructure development and training programs.",
+    description: "Works to improve living conditions and economic opportunities in urban and rural communities, through
+    infrastructure development and training programs.",
     cause: "Community Development",
     image: "community-development.png"
   },
   {
     name: "Bridge of Hope",
     address: "Kigali, Rwanda",
-    description: "Works to provide education and healthcare services to children affected by HIV/AIDS, and supports community-based initiatives to promote social inclusion.",
+    description: "Works to provide education and healthcare services to children affected by HIV/AIDS, and supports
+    community-based initiatives to promote social inclusion.",
     cause: "Health",
     image: "bridge-of-hope.png"
   },
   {
     name: "Animal Welfare Association",
     address: "Lahore, Pakistan",
-    description: "Animal Welfare Association is an organization in Pakistan that works to promote the welfare and rights of animals, including pets. They provide veterinary care, food, and shelter to animals in need, and also work to raise awareness about animal welfare issues.",
+    description: "Animal Welfare Association is an organization in Pakistan that works to promote the welfare and
+    rights of animals, including pets. They provide veterinary care, food, and shelter to animals in need, and also work
+    to raise awareness about animal welfare issues.",
     cause: "Animal Welfare",
     image: "animal-welfare-association.png"
   },
   {
     name: "Prevention of Cruelty to Animals",
     address: "Mumbai, India",
-    description: "The Society for the Prevention of Cruelty to Animals is an animal welfare organization in India that works to protect and care for animals, including pets. They rescue and rehabilitate injured and sick animals, and also work to educate the public about animal welfare issues.",
+    description: "The Society for the Prevention of Cruelty to Animals is an animal welfare organization
+    in India that works to protect and care for animals, including pets. They rescue and rehabilitate injured and
+    sick animals, and also work to educate the public about animal welfare issues.",
     cause: "Animal Welfare",
     image: "prevention-of-cruelty-to-animals.png"
   },
   {
     name: "African Wildlife Foundation",
     address: "Nairobi, Kenya",
-    description: "The African Wildlife Foundation is an organization in Kenya that works to protect and conserve wildlife in Africa, including endangered species. They also work to promote sustainable development in local communities and educate the public about conservation issues.",
+    description: "The African Wildlife Foundation is an organization in Kenya that works to protect and conserve
+    wildlife in Africa, including endangered species. They also work to promote sustainable development in
+    local communities and educate the public about conservation issues.",
     cause: "Animal Welfare",
     image: "african-wildlife-foundation.png"
   }
